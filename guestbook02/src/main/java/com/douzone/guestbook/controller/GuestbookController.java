0c912c4aaf74c7de0e92fa0acacbcd6e59584603
R35 <R35@DESKTOP-C4F52MO>
2020-02-11 13:43:17 +0900

    guestbook02 & jstlel 추가

---
+++ guestbook02/src/main/java/com/douzone/guestbook/controller/GuestbookController.java
@@ -0,0 +1,31 @@
+package com.douzone.guestbook.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GuestbookController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		
+		String action = request.getParameter("a");
+		
+		if("deleteform".equals(action)) {
+			
+		} else if("delete".equals(action)) {
+			
+		} else {
+			// default 요청 처리 (list)
+			request.getRequestDispatcher("/WEB-INF/views/guestbook/list.jsp").forward(request, response);
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
